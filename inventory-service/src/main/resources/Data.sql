e13ba90cf97354c887fc54b5f8b21d9b8c0d4cfc
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2020-07-07 10:39:01 +0530

    Data.sql

---
+++ inventory-service/src/main/resources/Data.sql
@@ -0,0 +1,9 @@
+  
+DELETE FROM inventory;
+
+insert into inventory(id, product_code, quantity) VALUES
+(1, 'P001', 250);
+insert into inventory(id, product_code, quantity) VALUES
+(2, 'P002', 132);
+insert into inventory(id, product_code, quantity) VALUES
+(3, 'P003', 0);
