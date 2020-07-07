59bc733b4c140272429f88acf2bbed89547e8f9c
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2020-07-07 10:33:35 +0530

    Product Controller class

---
+++ catalog-service/src/main/java/com/zeeshan/cs/controller/ProductController.java
@@ -0,0 +1,38 @@
+package com.zeeshan.cs.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zeeshan.cs.entity.Product;
+import com.zeeshan.cs.exception.ProductNotFoundException;
+import com.zeeshan.cs.service.ProductService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/api/products")
+@Slf4j
+public class ProductController {
+
+	@Autowired
+	private ProductService productService;
+
+	@GetMapping("")
+	public List<Product> allProducts() {
+
+		return productService.findAllProducts();
+	}
+	
+	@GetMapping("/{code}")
+	public Product productByCode(@PathVariable String code) {
+		
+		return productService.findProductByCode(code)
+					.orElseThrow(() -> new ProductNotFoundException("Product with code ["+code+"] doesn't exist"));
+	}
+	
+}
