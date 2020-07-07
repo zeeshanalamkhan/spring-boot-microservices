3ca718bb2d8bd3ff9b0039449ea2fc5823ea8216
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2020-07-07 10:35:10 +0530

    schema.sql

---
+++ catalog-service/src/main/resources/schema.sql
@@ -0,0 +1,3 @@
+insert into products(id, code, name, description, price) VALUES (1, 'P001', 'Product 1', 'Product 1 description', 25);
+insert into products(id, code, name, description, price) VALUES (2, 'P002', 'Product 2', 'Product 2 description', 32);
+insert into products(id, code, name, description, price) VALUES (3, 'P003', 'Product 3', 'Product 3 description', 50);
