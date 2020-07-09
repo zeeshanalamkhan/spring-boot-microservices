59eb0ffad942b94d2e6fb7419f2b386d8eb26cec
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2020-07-09 18:43:04 +0530

    Hystrix support added

--- catalog-service/src/main/java/com/zeeshan/cs/service/ProductServiceImpl.java
+++ catalog-service/src/main/java/com/zeeshan/cs/service/ProductServiceImpl.java
@@ -2,6 +2,7 @@ package com.zeeshan.cs.service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 
 import com.zeeshan.cs.entity.Product;
 import com.zeeshan.cs.repository.ProductRepository;
+import com.zeeshan.cs.utils.MyThreadLocalsHolder;
 import com.zeeshan.cs.web.ProductInventoryResponse;
 
 import lombok.extern.slf4j.Slf4j;
@@ -23,7 +25,6 @@ public class ProductServiceImpl implements ProductService {
 	@Autowired
 	private ProductRepository productRepository;
 
-	
 	@Autowired
 	private RestTemplate restTemplate;
 
@@ -41,34 +42,49 @@ public class ProductServiceImpl implements ProductService {
 	public Optional<Product> findProductByCode(String code) {
 
 		Optional<Product> product = productRepository.findByCode(code);
-		/*
-		 * 
-		 * if (product.isPresent()) {
-		 * 
-		 * log.info("Fetching inventory level for product_code: " + code);
-		 * ResponseEntity<ProductInventoryResponse> itemResponseEntity =
-		 * restTemplate.getForEntity( "http://inventory-service/api/inventory/{code}",
-		 * ProductInventoryResponse.class, code);
-		 * 
-		 * if (itemResponseEntity.getStatusCode() == HttpStatus.OK) {
-		 * 
-		 * Integer quantity = itemResponseEntity.getBody().getAvailableQuantity();
-		 * log.info("Available quantity: " + quantity);
-		 * 
-		 * product.get().setInStock(quantity > 0); } else {
-		 * log.error("Unable to get inventory level for product_code: " + code +
-		 * ", StatusCode: " + itemResponseEntity.getStatusCode()); } }
-		 * 
-		 */
-		
-		Optional<ProductInventoryResponse>  itemResponseEntity = inventoryServiceClient.getProductInvemtoryByCode(code);
-		
-		if(itemResponseEntity.isPresent()) {
-			
-			Integer quantity = itemResponseEntity.get().getAvailableQuantity();
-			product.get().setInStock(quantity > 0);
-		} 
-		
+
+		if (product.isPresent()) {
+
+			String correlationId = UUID.randomUUID().toString();
+			MyThreadLocalsHolder.setCorrelationId(correlationId);
+
+			/*
+			 * 
+			 * if (product.isPresent()) {
+			 * 
+			 * log.info("Fetching inventory level for product_code: " + code);
+			 * ResponseEntity<ProductInventoryResponse> itemResponseEntity =
+			 * restTemplate.getForEntity( "http://inventory-service/api/inventory/{code}",
+			 * ProductInventoryResponse.class, code);
+			 * 
+			 * if (itemResponseEntity.getStatusCode() == HttpStatus.OK) {
+			 * 
+			 * Integer quantity = itemResponseEntity.getBody().getAvailableQuantity();
+			 * log.info("Available quantity: " + quantity);
+			 * 
+			 * product.get().setInStock(quantity > 0); } else {
+			 * log.error("Unable to get inventory level for product_code: " + code +
+			 * ", StatusCode: " + itemResponseEntity.getStatusCode()); } }
+			 * 
+			 */
+
+			log.info("Before CorrelationID: " + MyThreadLocalsHolder.getCorrelationId());
+
+			log.info("Fetching inventory level for product_code: " + code);
+
+			Optional<ProductInventoryResponse> itemResponseEntity = inventoryServiceClient
+					.getProductInvemtoryByCode(code);
+
+			if (itemResponseEntity.isPresent()) {
+
+				Integer quantity = itemResponseEntity.get().getAvailableQuantity();
+				product.get().setInStock(quantity > 0);
+
+			}
+
+			log.info("After CorrelationID: " + MyThreadLocalsHolder.getCorrelationId());
+
+		}
 		return product;
 	}
 
