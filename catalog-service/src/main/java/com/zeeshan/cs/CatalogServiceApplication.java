9c0a6607aefe8eb8868599f9b23e10f399a03cc8
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2020-07-13 16:10:45 +0530

    FeignClient enabled

--- catalog-service/src/main/java/com/zeeshan/cs/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/zeeshan/cs/CatalogServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @EnableCircuitBreaker
 @EnableHystrixDashboard
+@EnableFeignClients
 public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
